042c0bd8b890dfe204518cf296e1a552e6cf4c78
Sunil Sahu <kumarsunilsahu31@gmail.com>
2025-05-25 18:44:38 +0530

    Delete 3.py

--- 3.py
+++
@@ -1,8 +0,0 @@
-n = int(input("Enter how many numbers you want to enter "))
-i = 0
-sum = 0
-while i < n:
-    a = int(input(f"Enter number {i+1}: "))
-    sum= sum + a
-    i += 1
-print("Total sum:", sum)
