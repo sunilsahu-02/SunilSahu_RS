689cfefd89369162a0d28a7ce415e141cd1fe917
Sunil Sahu <kumarsunilsahu31@gmail.com>
2025-05-25 18:44:50 +0530

    Delete 6.py

--- 6.py
+++
@@ -1,8 +0,0 @@
-L= ['a', 'b', 'c','d','e']
-D = {'a': 11, 'b': 22, 'm': 33, 'd': 66}
-key = input("Enter key: ")
-
-if key in L and key in D:
-    print("Value of key:", D[key])
-else:
-    print("Key not present.")
